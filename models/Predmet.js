7d148ae24983fdd852a474a214821944d3ab78bf
Srdjan Stojanac <stojanac.srdjan@gmail.com>
2025-07-18 18:11:50 +0200

    statistika model

--- models/Predmet.js
+++ models/Predmet.js
@@ -25,7 +25,8 @@ class Predmet {
   async dodajPredmet() {
     try {
       let pool = await getPool();
-      let upit = "INSERT INTO predmeti (broj_predmeta, ime_stranke, referent, datum_podnosenja, datum_pravosnaznosti, napomena, status, korisnik_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+      let upit =
+        "INSERT INTO predmeti (broj_predmeta, ime_stranke, referent, datum_podnosenja, datum_pravosnaznosti, napomena, status, korisnik_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
       let [rezultat] = await pool.query(upit, [
         this.broj_predmeta,
         this.stranka,
@@ -44,7 +45,8 @@ class Predmet {
 
   static async sviPredmetiKorisnika(korisnikId) {
     const pool = await getPool();
-    const upit = "SELECT * FROM predmeti WHERE korisnik_id = ? ORDER BY datum_unosa DESC";
+    const upit =
+      "SELECT * FROM predmeti WHERE korisnik_id = ? ORDER BY datum_unosa DESC";
     const [rezultat] = await pool.query(upit, [korisnikId]);
     return rezultat;
   }
@@ -65,94 +67,150 @@ class Predmet {
   }
 
   static async posebnaPretraga(...args) {
-  try {
-    const pool = await getPool();
-    let uslovi = [];
-    let vrednosti = [];
+    try {
+      const pool = await getPool();
+      let uslovi = [];
+      let vrednosti = [];
 
-    args.forEach(([polje, vrednost]) => {
-      if (polje === "broj_predmeta" || polje === "ime_stranke" || polje === "referent") {
-        uslovi.push(`${polje} LIKE ?`);
-        vrednosti.push(`%${vrednost}%`);
-      } else {
-        uslovi.push(`${polje} = ?`);
-        vrednosti.push(vrednost);
+      args.forEach(([polje, vrednost]) => {
+        if (
+          polje === "broj_predmeta" ||
+          polje === "ime_stranke" ||
+          polje === "referent"
+        ) {
+          uslovi.push(`${polje} LIKE ?`);
+          vrednosti.push(`%${vrednost}%`);
+        } else {
+          uslovi.push(`${polje} = ?`);
+          vrednosti.push(vrednost);
+        }
+      });
+
+      const uslovniUpit =
+        uslovi.length > 0 ? `WHERE ${uslovi.join(" AND ")}` : "";
+      const upit = `SELECT * FROM predmeti ${uslovniUpit}`;
+      const [rezultat] = await pool.query(upit, vrednosti);
+
+      return rezultat;
+    } catch (error) {
+      console.error("Greska u posebnaPretraga:", error);
+      throw new AppError("Greska u funkciji posebnaPretraga!", 500);
+    }
+  }
+
+  static async statistikaGodina(ID) {
+    try {
+      let datum = new Date();
+      let godina = datum.getFullYear();
+      let danas = datum.toISOString().slice(0, 10);
+      let pocetakGodine = `${godina}-01-01`;
+
+      let pool = await getPool();
+      let upit = `
+      SELECT COUNT(*) AS count FROM predmeti 
+      WHERE datum_unosa BETWEEN ? AND ? AND id = ?
+    `;
+
+      let [[{ count }]] = await pool.query(upit, [pocetakGodine, danas, ID]);
+      return count || false;
+    } catch (error) {
+      console.error("Greska u statistikaGodina:", error);
+      throw new AppError("Greska u funkciji statistikaGodina!", 500);
+    }
+  }
+
+  static async statistikaMesec(ID) {
+    try {
+      let datum = new Date();
+      let godina = datum.getFullYear();
+      let mesec = String(datum.getMonth() + 1).padStart(2, "0");
+      let danas = datum.toISOString().slice(0, 10);
+      let pocetakMeseca = `${godina}-${mesec}-01`;
+
+      let pool = await getPool();
+      let upit = `
+      SELECT COUNT(*) AS count FROM predmeti 
+      WHERE datum_unosa BETWEEN ? AND ? AND id = ?
+    `;
+
+      let [[{ count }]] = await pool.query(upit, [pocetakMeseca, danas, ID]);
+      return count || false;
+    } catch (error) {
+      console.error("Greska u statistikaMesec:", error);
+      throw new AppError("Greska u funkciji statistikaMesec!", 500);
+    }
+  }
+
+  static async statistikaDan(ID) {
+    try {
+      let datum = new Date();
+      let danas = datum.toISOString().slice(0, 10);
+
+      let pool = await getPool();
+      let upit = `
+      SELECT COUNT(*) AS count FROM predmeti 
+      WHERE DATE(datum_unosa) = ? AND id = ?
+    `;
+
+      let [[{ count }]] = await pool.query(upit, [danas, ID]);
+      return count || false;
+    } catch (error) {
+      console.error("Greska u statistikaDan:", error);
+      throw new AppError("Greska u funkciji statistikaDan!", 500);
+    }
+  }
+
+  static async statistikaNedelja(ID) {
+    try {
+      let datum = new Date();
+      let danas = datum.toISOString().slice(0, 10);
+
+      let preSedamDana = new Date(datum);
+      preSedamDana.setDate(preSedamDana.getDate() - 7);
+      let sedamDanaUnazad = preSedamDana.toISOString().slice(0, 10);
+
+      let pool = await getPool();
+      let upit = `
+      SELECT COUNT(*) AS count FROM predmeti 
+      WHERE datum_unosa BETWEEN ? AND ? AND id = ?
+    `;
+
+      let [[{ count }]] = await pool.query(upit, [sedamDanaUnazad, danas, ID]);
+      return count || false;
+    } catch (error) {
+      console.error("Greska u statistikaNedelja:", error);
+      throw new AppError("Greska u funkciji statistikaNedelja!", 500);
+    }
+  }
+
+  static async statistikaNedeljaPoDanima(ID) {
+    let nedelja = {};
+    let datum = new Date();
+    let sekunde = 86400 * 1000;
+
+    try {
+      let pool = await getPool();
+      let upit, index;
+
+      for (let i = 0; i < 7; i++) {
+        let targetDate = new Date(datum.getTime() - i * sekunde);
+        let danas = targetDate.toISOString().slice(0, 10);
+
+        upit = `
+        SELECT COUNT(*) AS count FROM predmeti 
+        WHERE DATE(datum_unosa) = ? AND id = ?
+      `;
+        let [[{ count }]] = await pool.query(upit, [danas, ID]);
+        index = i + 1;
+        nedelja[index] = count;
       }
-    });
 
-    const uslovniUpit = uslovi.length > 0 ? `WHERE ${uslovi.join(" AND ")}` : "";
-    const upit = `SELECT * FROM predmeti ${uslovniUpit}`;
-    const [rezultat] = await pool.query(upit, vrednosti);
-
-    return rezultat;
-  } catch (error) {
-    console.error("Greska u posebnaPretraga:", error);
-    throw new AppError("Greska u funkciji posebnaPretraga!", 500);
+      return nedelja || false;
+    } catch (error) {
+      console.error("Greska u statistikaNedeljaPoDanima:", error);
+      throw new AppError("Greska u funkciji statistikaNedeljaPoDanima!", 500);
+    }
   }
-}
-
-static async statistikaGodina() {
-  try {
-    let datum = new Date();
-    let godina = datum.getFullYear();
-    let danas = datum.toISOString().slice(0, 10);
-    let pocetakGodine = `${godina}-01-01`;
-
-    let pool = await getPool();
-    let upit = `
-      SELECT * FROM predmeti 
-      WHERE datum_unosa BETWEEN ? AND ?
-    `;
-
-    let [rezultat] = await pool.query(upit, [pocetakGodine, danas]);
-    return rezultat || false;
-  } catch (error) {
-    console.error("Greska u statistikaGodina:", error);
-    throw new AppError("Greska u funkciji statistikaGodina!", 500);
-  }
-}
-
-static async statistikaMesec() {
-  try {
-    let datum = new Date();
-    let godina = datum.getFullYear();
-    let mesec = String(datum.getMonth() + 1).padStart(2, '0');
-    let danas = datum.toISOString().slice(0, 10);
-    let pocetakMeseca = `${godina}-${mesec}-01`;
-
-    let pool = await getPool();
-    let upit = `
-      SELECT * FROM predmeti 
-      WHERE datum_unosa BETWEEN ? AND ?
-    `;
-
-    let [rezultat] = await pool.query(upit, [pocetakMeseca, danas]);
-    return rezultat || false;
-  } catch (error) {
-    console.error("Greska u statistikaMesec:", error);
-    throw new AppError("Greska u funkciji statistikaMesec!", 500);
-  }
-}
-
-static async statistikaDan() {
-  try {
-    let datum = new Date();
-    let danas = datum.toISOString().slice(0, 10);
-
-    let pool = await getPool();
-    let upit = `
-      SELECT * FROM predmeti 
-      WHERE DATE(datum_unosa) = ?
-    `;
-
-    let [rezultat] = await pool.query(upit, [danas]);
-    return rezultat || false;
-  } catch (error) {
-    console.error("Greska u statistikaDan:", error);
-    throw new AppError("Greska u funkciji statistikaDan!", 500);
-  }
-}
-
 }
 
 module.exports = Predmet;
